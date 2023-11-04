907358d45841e1bc8599181dfaaf2a967336a9e2
eubentof <dev.bento@gmail.com>
2023-11-04 20:28:48 +0000

    fix lint bugs

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 import {
-	App,
-	Editor,
 	MarkdownView,
-	Modal,
-	Notice,
 	Plugin,
-	PluginSettingTab,
-	Setting,
-	TAbstractFile,
 	TFile,
 } from "obsidian";
 
@@ -36,66 +29,6 @@ export default class TimestempPlugin extends Plugin {
 			},
 		});
 
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-		// // This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {}
@@ -128,8 +61,8 @@ export default class TimestempPlugin extends Plugin {
 		if (activeFile.basename === currentDate) {
 			editor.replaceSelection(`###### ⏲ *${currentDate} ${currentTime}*`);
 		} else {
-			const dailyNotePath: string = `Journal/Daily/${currentDate}`;
-			const linkText: string = `[[${dailyNotePath}#^${timestamp}|${currentDate} ${currentTime}]]`;
+			const dailyNotePath = `Journal/Daily/${currentDate}`;
+			const linkText = `[[${dailyNotePath}#^${timestamp}|${currentDate} ${currentTime}]]`;
 			editor.replaceSelection(`###### ⏲ *${linkText}*`);
 			await this.insertLineInDailyNote(
 				currentDate,
@@ -143,69 +76,29 @@ export default class TimestempPlugin extends Plugin {
 		currentDate: string,
 		originatingFileName: string,
 		timestamp: number
-	  ): Promise<void> {
-		const dailyNotePath: string = `Journal/Daily/${currentDate}.md`;
-		let dailyNote = this.app.vault.getAbstractFileByPath(dailyNotePath);
-	  
+	): Promise<void> {
+		const dailyNotePath = `Journal/Daily/${currentDate}.md`;
+		const dailyNote = this.app.vault.getAbstractFileByPath(dailyNotePath);
+
 		// Check if the abstract file is indeed a file, not a folder
 		if (dailyNote instanceof TFile) {
-		  const fileContents: string = await this.app.vault.read(dailyNote);
-		  const headerReference: string = `${currentDate}${timestamp.toString().slice(0, -3)}`;
-		  const lineToAdd: string = `![[${originatingFileName}#⏲ * Journal/Daily/${currentDate} ${headerReference} ${currentDate.replace(/-/g, ' ')} *]]\n`;
-	  
-		  if (!fileContents.includes(lineToAdd)) {
-			await this.app.vault.modify(dailyNote, fileContents + '\n' + lineToAdd);
-		  }
+			const fileContents: string = await this.app.vault.read(dailyNote);
+			const headerReference = `${currentDate}${timestamp
+				.toString()
+				.slice(0, -3)}`;
+			const lineToAdd = `![[${originatingFileName}#⏲ * Journal/Daily/${currentDate} ${headerReference} ${currentDate.replace(
+				/-/g,
+				" "
+			)} *]]\n`;
+
+			if (!fileContents.includes(lineToAdd)) {
+				await this.app.vault.modify(
+					dailyNote,
+					fileContents + "\n" + lineToAdd
+				);
+			}
 		} else {
-		  console.error('The daily note is not a file: ', dailyNotePath);
+			console.error("The daily note is not a file: ", dailyNotePath);
 		}
-	  }
-	  
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: TimestempPlugin;
-
-	constructor(app: App, plugin: TimestempPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
 	}
 }
